a4c0a611e240a2a9f949ba5a685f4d7d7a0ce4e7
Muhammad Sultan <125191342+Muhammad-Sultan-56@users.noreply.github.com>
2023-08-15 09:07:53 +0500

    FAQ Completed
    FAQ Section completed and Responsiveness added in the Website.

---
+++ js/script.js
@@ -0,0 +1,105 @@
+// Question 1 function Start
+
+
+function ans1(){
+    let ans1 = document.getElementById("ans1");
+
+    if(ans1.style.display = "none"){
+        ans1.style.display = "block";
+    }
+}
+
+function ans1Hide(){
+    let ans1 = document.getElementById("ans1");
+
+    if(ans1.style.display = "block"){
+        ans1.style.display = "none";
+
+    }
+}
+
+// Question 1 function End
+
+// Question 2 function Start
+
+function ans2(){
+    
+    let ans2 = document.getElementById("ans2");
+
+    if(ans2.style.display = "none"){
+        ans2.style.display = "block";
+    }
+}
+function ans2Hide(){
+    let ans2 = document.getElementById("ans2");
+
+    if(ans2.style.display = "block"){
+        ans2.style.display = "none";
+
+    }
+}
+// Question 2 function End
+
+// Question 3 function Start
+
+function ans3(){
+    
+    let ans3 = document.getElementById("ans3");
+
+    if(ans3.style.display = "none"){
+        ans3.style.display = "block";
+    }
+}
+function ans3Hide(){
+    let ans3 = document.getElementById("ans3");
+
+    if(ans3.style.display = "block"){
+        ans3.style.display = "none";
+
+    }
+}
+
+// Question 3 function End
+
+// Question 4 function Start
+
+function ans4(){
+    
+    let ans4 = document.getElementById("ans4");
+
+    if(ans4.style.display = "none"){
+        ans4.style.display = "block";
+    }
+}
+
+function ans4Hide(){
+    let ans4 = document.getElementById("ans4");
+
+    if(ans4.style.display = "block"){
+        ans4.style.display = "none";
+
+    }
+}
+
+// Question 4 function End
+
+// Question 5 function Start
+
+function ans5(){
+    
+    let ans5 = document.getElementById("ans5");
+
+    if(ans5.style.display = "none"){
+        ans5.style.display = "block";
+    }
+}
+function ans5Hide(){
+    let ans5 = document.getElementById("ans5");
+
+    if(ans5.style.display = "block"){
+        ans5.style.display = "none";
+
+    }
+}
+
+// Question 5 function End
